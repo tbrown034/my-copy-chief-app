566311da3713bbb45f13a5fffc9709229ad2fc3f
tbrown034 <tbrown034@gmail.com>
2024-01-20 21:06:38 -0500

    refractoring starting with folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
-import { useState } from "react";
-import MainMenu from "./components/UI/MainMenu";
-import GameMenu from "./components/UI/GameMenu";
 function App() {
-  const [showGameMenu, setShowGameMenu] = useState(false);
-
-  const startGame = () => {
-    setShowGameMenu(true);
-  };
-
-  const backToMenu = () => {
-    setShowGameMenu(false);
-  };
-
-  return (
-    <div className="p-2 px-4 bg-zinc-200">
-      {!showGameMenu ? (
-        <MainMenu startGame={startGame} />
-      ) : (
-        <GameMenu backToMenu={backToMenu} />
-      )}
-    </div>
-  );
+  return <div className="min-h-screen p-2 px-4 bg-zinc-200">hi</div>;
 }
 
 export default App;
