3076883112e1ca71d0cd2b51f25ac1b286386dd5
tbrown034 <tbrown034@gmail.com>
2024-01-20 21:24:49 -0500

    basic UI structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Footer from "./components/UI/Footer";
+import Header from "./components/UI/Header";
+import Game from "./components/UI/Game";
+import Home from "./components/UI/Home";
+
 function App() {
-  return <div className="min-h-screen p-2 px-4 bg-zinc-200">hi</div>;
+  return (
+    <div className="flex flex-col min-h-screen gap-4 p-4 bg-zinc-200">
+      <Header />
+      <Home />
+      <Game />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UI/Game.jsx
@@ -0,0 +1,7 @@
+export default function Game() {
+  return (
+    <>
+      <div>Game</div>
+    </>
+  );
+}
