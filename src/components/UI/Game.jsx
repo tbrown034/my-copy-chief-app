1a495d20b473291932e7ec6f2e6d080d1f0c201a
tbrown034 <tbrown034@gmail.com>
2024-01-24 16:53:31 -0500

    added show answers button and conditional rendering

--- src/components/UI/Game.jsx
+++ src/components/UI/Game.jsx
@@ -7,6 +7,7 @@ export default function Game({ setGameDisplay }) {
   const numOfNewsArticles = 2;
   const section = "home";
   const [fullArticles, setFullArticles] = useState([]); // Renamed for clarity
+  const [showAnswers, setShowAnswers] = useState(false);
 
   useEffect(() => {
     const loadArticles = async () => {
@@ -25,24 +26,29 @@ export default function Game({ setGameDisplay }) {
     setGameDisplay(false);
   };
 
+  const handleShowAnswers = () => {
+    setShowAnswers(true);
+  };
+
   return (
     <>
-      <div>Game</div>
-      <ul>
-        {fullArticles.map((article, index) => (
-          <li key={index}>
-            <h3>{article.title}</h3>
-            {/* Add more details as needed, like article.abstract */}
-          </li>
-        ))}
-      </ul>
       <button
         onClick={handleClick}
         className="p-2 px-12 text-lg bg-sky-900 hover:bg-sky-700 active:bg-sky-600 text-sky-100 rounded-xl"
       >
         Back to Home
       </button>
-      <Answers fullArticles={fullArticles} />
+      <button
+        onClick={handleShowAnswers}
+        className="p-2 px-12 text-lg bg-sky-900 hover:bg-sky-700 active:bg-sky-600 text-sky-100 rounded-xl"
+      >
+        Show Answers
+      </button>
+      {!showAnswers ? (
+        ""
+      ) : (
+        <Answers setShowAnswers={setShowAnswers} fullArticles={fullArticles} />
+      )}
     </>
   );
 }
--- src/components/gameElements/Answers.jsx
+++ src/components/gameElements/Answers.jsx
@@ -1,15 +1,25 @@
 // Answers.jsx
-const Answers = ({ fullArticles }) => {
+const Answers = ({ fullArticles, setShowAnswers }) => {
+  const handleClick = () => {
+    setShowAnswers(false);
+  };
+
   return (
     <div>
       {fullArticles.map((fullArticles, index) => (
         <div key={index}>
-          <h3>{fullArticles.title}</h3>
+          <a className="font-semibold" href={fullArticles.url}>
+            <h3 className="font-bold">{fullArticles.title}</h3>
+          </a>
           <p>{fullArticles.abstract}</p>
-          <p>By: {fullArticles.byline}</p>
-          <a href={fullArticles.url}>Read More</a>
         </div>
       ))}
+      <button
+        onClick={handleClick}
+        className="p-2 px-12 text-lg bg-sky-900 hover:bg-sky-700 active:bg-sky-600 text-sky-100 rounded-xl"
+      >
+        Back to Game
+      </button>
     </div>
   );
 };
